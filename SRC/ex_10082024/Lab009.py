9ac271ef10b434eb010aa269e0f50dc785d9937c
Rekhaajadhav <rekhaajadhav26git config --global user.email rekhaajadhav26@gmail.com>
2024-08-10 21:23:54 +0530

    practice of keywords,identifires,commands and datatype on 10th august2024

--- SRC/ex_10082024/Lab009.py
+++ SRC/ex_10082024/Lab009.py
@@ -3,12 +3,12 @@
 # At the run time, data of the variable can be changed. and you don't need to
 # mention the data type
 
-#debug
-#click on line number (basically make it red )
-#click on dubug button near run button
+# debug
+# click on line number (basically make it red )
+# click on debug button near run button
 
 
-x=56
+x = 56
 print(type(x))
 x = "Dynamic Type"
 print(x)
@@ -20,8 +20,8 @@ c = 10
 sum = a + b + c
 sum = sum - 11
 sum = sum + 1
-sum=sum+1
+sum = sum + 1
 print(sum)
 
-x,y,z=76,34,23
-print(x,y,z, sep=",")
+x, y, z = 76, 34, 23
+print(x, y, z, sep=",")
